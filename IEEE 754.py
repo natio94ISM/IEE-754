1e2282dc7819e93fbc42473148729d5620d0e2db
antho12344556676 <90260466+antho12344556676@users.noreply.github.com>
2021-11-11 20:01:53 +0100

    Update IEEE 754.py

--- IEEE 754.py
+++ IEEE 754.py
@@ -28,24 +28,23 @@ def base2_16(chaine_nb_base_2):
   chaine = hex(int(chaine_nb_base_2,2))[2:]
   return calibre_gauche(chaine,8)
 
-def base10_to_2_decimal(x):
-  a=""
-  if 0<=x<1:
-    while x!=0:
-      x=x * 2
-      x=str(x)
-      a=a+x[0:1]
-      y=int(x[0:1])
-      x=float(x)
-      x=x-y
-      if len(a)>23:
+def base10_to_2_decimal(Float,nb_chiffres_ap_virg):
+  nb_chiffres_ap_virg=""
+  if 0<=Float<1:
+    while xFloat!=0:
+      Float=Float * 2
+      Float=str(Float)
+      nb_chiffres_ap_virg=nb_chiffres_ap_virg+Float[0:1]
+      y=int(Float[0:1])
+      Float=float(Float)
+      Float=Float-y
+      if len(nb_chiffres_ap_virg)>23:
         break
-      while len(a[0:1]) == 0:
+      while len(anb_chiffres_ap_virg[0:1]) == 0:
         continue
-  return a
+  return nb_chiffres_ap_virg
 
-def calibre_droite(x):
-  x="0"
-  while len(x)<23:
-    x=x+x
-  return x
+def calibre_droite(chaine,nb_car):
+  while len(chaine)<23:
+    nb_car=chaine+"0"  
+  return nb_car
