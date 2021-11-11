d576ba027eb588aca97fa8607a3ad29face16075
FloraBoulanger <90260328+FloraBoulanger@users.noreply.github.com>
2021-11-11 19:27:48 +0100

    Create IEEE 754.py

--- IEEE 754.py
+++ IEEE 754.py
@@ -1,7 +1,18 @@
-def puissance_de_deux(nb_decimal):
-    for i in range(-128,127):
-        a=2**i
-        if a >= nb_decimal:
-            n=i
-            break
-    return n
+
+def signe(nb_decimal):
+  if nb_decimal >=0: return "0"
+  else: return "1"
+  
+def base10_2_entier(entier_base10):
+  if entier_base10 == 0:
+    resultat = "0"
+  else:
+    resultat = ""
+    while entier_base10 != 0:
+      resultat = "01" [entier_base10 % 2] + resultat
+      entier_base10 = entier_base10//2
+  return resultat
+
+def base2_16(chaine_nb_base_2):
+  chaine = hex(int(chaine_nb_base_2,2))[2:]
+  return calibre_gauche(chaine,8)
